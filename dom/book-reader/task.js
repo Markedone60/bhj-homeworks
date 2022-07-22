b6fba549ae54d9e65ec0df8f51436719290389e2
Grigory <gregory.rvl@yandex.ru>
2022-07-22 21:59:41 +0300

    Initial commit

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -2,3 +2,20 @@
 
 const book = document.getElementById('book');
 const fontsArr = Array.from(document.querySelectorAll('.font-size'));
+const content = document.querySelector('.book__content');
+let index = fontsArr.findIndex((elem) => elem.classList.contains('font-size_active'));
+
+const fontSwitch = function (event) {
+    
+    fontsArr[index].classList.remove('font-size_active');
+    event.preventDefault();
+
+    if (event.target.className.includes('font-size_big')) {
+        book.classList.add('book_fs-big');
+    } else if (event.target.className.includes('font-size_small')) {
+        book.classList.add('book_fs-small');
+    } else 
+        book.classList.remove('book_fs-big', 'book_fs-small');
+}
+
+document.addEventListener('click', fontSwitch);
