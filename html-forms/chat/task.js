7d791d6e052ef5e8c028fb59023c620191b3f076
Grigory <gregory.rvl@yandex.ru>
2022-07-23 20:48:04 +0300

    Intermediary commit

---
+++ html-forms/chat/task.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const btnChat = document.querySelector('.chat-widget__side');
+const chat = document.querySelector('.chat-widget');
+
+const messages = document.querySelector('.chat-widget__messages');
+const input = document.querySelector('chat-widget__input');
+
+const botAnswers = ['Привет!', 'Я вас слушаю', 'Что хотите узнать?', 'Бла бла бла'];
+
+function getTime() {
+
+}
+
+function sendMessageBot() {
+
+}
+
+function sendMessageUser() {
+    
+}
