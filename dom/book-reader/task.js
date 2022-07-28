df81f77e33f78ab6696cdef2414bb19101870149
Grigory <gregory.rvl@yandex.ru>
2022-07-28 11:23:26 +0300

    Third commit - changes in book-reader

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,23 +1,33 @@
 'use strict';
 
 const book = document.getElementById('book');
-const fontsArr = Array.from(document.querySelectorAll('.font-size'));
+const fontsArr = Array.from(document.querySelectorAll('a.font-size'));
 const content = document.querySelector('.book__content');
-let index = fontsArr.findIndex((elem) => elem.classList.contains('font-size_active'));
+
+
 
 const fontSwitch = function (event) {
-    
-    fontsArr[index].classList.remove('font-size_active');
-    event.preventDefault();
 
-    if (event.target.className.includes('font-size_big')) {
+    event.preventDefault();
+    removeCircle();
+
+    if (event.target.classList.contains('font-size_big')) {
         book.classList.add('book_fs-big');
         book.classList.remove('book_fs-small');
-    } else if (event.target.className.includes('font-size_small')) {
+        event.target.classList.add('font-size_active');
+    } else if (event.target.classList.contains('font-size_small')) {
         book.classList.add('book_fs-small');
         book.classList.remove('book_fs-big');
+        event.target.classList.add('font-size_active');
     } else 
         book.classList.remove('book_fs-big', 'book_fs-small');
+        event.target.classList.add('font-size_active');
+}
+
+function removeCircle() {
+    for (let i = 0; i < fontsArr.length; i++) {
+        fontsArr[i].classList.remove('font-size_active');
+    }
 }
 
 document.addEventListener('click', fontSwitch);
