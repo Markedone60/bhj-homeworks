9df55b640986e2aa4fbfa15d9cb53425aba47c56
Grigory <gregory.rvl@yandex.ru>
2022-08-04 22:48:01 +0300

    Second commit

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -23,81 +23,32 @@ valueDec.forEach((dec) => {
   })
 });
 
+function productBody(id, image, quantity){
+  const productBody = document.createElement('div');
+  productBody.classList.add('cart__product');
+  productBody.dataset['id'] = id;
+  productBody.innerHTML = `<img class="cart__product-image" src="${image}">
+  <div class="cart__product-count">${quantity}</div>
+  `;
+  return productBody;
+}
+
 
 productAdd.forEach((btn) => {
   btn.addEventListener('click', (event) => {
     event.preventDefault();
 
+    const product = event.target.closest('.product');
+    const productQty = Number(product.querySelector('div.product__quantity-value').textContent);
+    const productImg = product.querySelector('img.product__image').src;
     const cart = document.querySelector('.cart__products');
-    const cartArr = Array.from(document.querySelectorAll('.cart__product'));
-    const findActive = cartArr.find(item => item.classList.contains("tooltip_active"));
-    const product = this.closest('.product');
-
-    if (btn.closest('.product').dataset.id === findActive.dataset.id) {
-      item.querySelector('.cart__product-count').textContent = Number(item.querySelector('.cart__product-count').textContent) +
-        Number(btn.closest('.product').querySelector('.product__quantity-value').textContent);
+    const productInCart = Array.from(cart.children).find(elem => elem.dataset['id'] === product.dataset['id']);
+      
+    if (productInCart) {
+      const productInCartQty = Number(productInCart.querySelector('.cart__product-count').textContent);
+      cart.replaceChild(productBody(product.dataset['id'], productImg, productQty + productInCartQty), productInCart);
     } else {
-      cart.insertAdjacentHTML('afterBegin', `<div class="cart__product" data-id="${btn.closest('.product').dataset.id}">
-                <img class="cart__product-image" src="${btn.closest('.product').querySelector('.product__image').src}">
-                <div class="cart__product-count">${btn.closest('.product').querySelector('.product__quantity-value').textContent}</div>
-                </div>`);
+      cart.appendChild(productBody(product.dataset['id'], productImg, productQty));
     }
   })
 })
-
-
-
-function addToBasket(){
-  let product = this.closest('.product');
-  let productQuantity = Number(product.querySelector('div.product__quantity-value').textContent);
-  let productImage = product.querySelector('img.product__image').src;
-  let cart = document.querySelector('div.cart__products');
-  let productExist = Array.from(cart.children).find(el => el.dataset['id'] == product.dataset['id']);       //Не строго чтобы не преобразовывать типы
-
-  if(productExist){
-    let productExistQuantity = Number(productExist.querySelector('.cart__product-count').textContent);
-    cart.replaceChild(productNode(product.dataset['id'], productImage, productQuantity + productExistQuantity), productExist);
-  } else{
-    cart.appendChild(productNode(product.dataset['id'], productImage, productQuantity));
-  }
-}
-
-/*
-
-productAdd.forEach((btn) => {
-  btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const cart = document.querySelector('.cart__products');
-    const cartArr = document.querySelectorAll('.cart__product');
-    
-    if (cart.children.length === 0) {
-      cart.insertAdjacentHTML('afterBegin', `<div class="cart__product" data-id="${btn.closest('.product').dataset.id}">
-        <img class="cart__product-image" src="${btn.closest('.product').querySelector('.product__image').src}">
-        <div class="cart__product-count">${btn.closest('.product').querySelector('.product__quantity-value').textContent}</div>
-        </div>`);
-    } else {
-      for (let i = 0; i < cartArr.length; i++) {
-        if (btn.closest('.product').dataset.id === cartArr[i].dataset.id) {
-          cartArr[i].querySelector('.cart__product-count').textContent = Number(cartArr[i].querySelector('.cart__product-count').textContent) +
-            Number(btn.closest('.product').querySelector('.product__quantity-value').textContent);
-        } else {
-          cart.insertAdjacentHTML('afterBegin', `<div class="cart__product" data-id="${btn.closest('.product').dataset.id}">
-                <img class="cart__product-image" src="${btn.closest('.product').querySelector('.product__image').src}">
-                <div class="cart__product-count">${btn.closest('.product').querySelector('.product__quantity-value').textContent}</div>
-                </div>`);
-        }
-      }
-    }
-  })
-})
-
-function toCart () {
-  const cart = document.querySelector('.cart__products');
-  const cartArr = Array.from(document.querySelectorAll('.cart__product'));
-
-  const findBtn = productAdd.find(btn => btn.classList.contains("tooltip_active"));
-  console.log(findBtn);
-}
-
-*/
-// не получается реализовать правильно условие добавление элемента в корзину при наличии этого же элемента внутри (элементы дублируются)
--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,57 +1,29 @@
 'use strict';
 
-const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltips = Array.from(document.querySelectorAll('.has-tooltip'));
 
-tooltips.forEach((tooltip) => {
-  tooltip.addEventListener('click', (event) => {
-    event.preventDefault();
-    const tipTitle = event.target.title;
-    const div = document.createElement('div');
-    div.classList.add('tooltip');
-    div.textContent = tipTitle;
-    event.target.insertAdjacentElement('afterEnd', div);
+tooltips.forEach(tooltip => {
+   const div = document.createElement('div');
+   div.textContent = tooltip.title;
 
-    div.style.left = `${tooltip.getBoundingClientRect().left}px`;
-    div.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
+   tooltip.addEventListener('click', (event) => {
+      event.preventDefault();
+      div.classList.add('tooltip');
+      tooltip.insertAdjacentElement('beforeEnd', div);
+      div.style.left = `${tooltip.getBoundingClientRect().left}px`;
+      div.style.top = `${tooltip.getBoundingClientRect().bottom}px`;
 
-    const activators = Array.from(document.querySelectorAll(".tooltip_active"));
-    const findDiv = activators.find(div => div.classList.contains("tooltip_active"));
+      const activators = Array.from(document.querySelectorAll('.tooltip_active'));
+      const findDiv = activators.find(div => div.classList.contains('tooltip_active'));
 
-    if (findDiv === undefined) {
-      div.classList.add("tooltip_active");
-    } else {
-      if (findDiv !== div) {
-        div.classList.add("tooltip_active");
+      if (findDiv === undefined) {
+         div.classList.add('tooltip_active');
+      } else {
+         if (findDiv !== div) {
+            div.classList.add('tooltip_active');
+         }
+         findDiv.classList.remove('tooltip_active');
       }
-      findDiv.classList.remove("tooltip_active");
-    }
-  })
+   }
+   )
 })
-
-function removeActive() {
-  const activators = Array.from(document.querySelectorAll(".tooltip_active"));
-  const findDiv = activators.find(div => div.classList.contains("tooltip_active"));
-
-  if (findDiv === undefined) {
-    div.classList.add("tooltip_active");
-  } else {
-    if (findDiv !== div) {
-      div.classList.add("tooltip_active");
-    }
-    findDiv.classList.remove("tooltip_active");
-  }
-
-
-}
-
-/*    if (findDiv === undefined) {
-      div.classList.add('tooltip_active');
-    } else {
-      if (findDiv !== div) {
-        div.classList.add('tooltip_active');
-      } findDiv.classList.remove('tooltip_active')
-    }
-*/
-
-
-//не получается реализовать снятие активного класса по клику
